556f84146d6f841aaf9980791297ddb5c686f874
Peter Benjamin <petermbenjamin@gmail.com>
2023-09-01 16:59:06 -0700

    fix: insert '-S -' if capture_history is true

--- lua/cmp_tmux/tmux.lua
+++ lua/cmp_tmux/tmux.lua
@@ -51,7 +51,7 @@ end
 function Tmux.create_pane_data_job(self, pane, on_data, on_exit)
     local cmd = { 'tmux', 'capture-pane', '-p', '-t', pane }
     if self.config.capture_history then
-        cmd = vim.tbl_extend('force', cmd, {'-S', '-'})
+        cmd = vim.insert(cmd, '-S -')
     end
 
     return vim.fn.jobstart(cmd, {
