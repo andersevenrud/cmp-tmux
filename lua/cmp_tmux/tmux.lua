dfc89374380bcebf1c6a76166ccaadd4e42fe5df
Peter Benjamin <petermbenjamin@gmail.com>
2023-09-01 17:04:47 -0700

    fix: no need to reassign

--- lua/cmp_tmux/tmux.lua
+++ lua/cmp_tmux/tmux.lua
@@ -51,7 +51,7 @@ end
 function Tmux.create_pane_data_job(self, pane, on_data, on_exit)
     local cmd = { 'tmux', 'capture-pane', '-p', '-t', pane }
     if self.config.capture_history then
-        cmd = table.insert(cmd, ' -S -')
+        table.insert(cmd, ' -S -')
     end
 
     return vim.fn.jobstart(cmd, {
